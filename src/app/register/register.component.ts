6ad2003f519f83a3512a74e48f7cfcd084b94a74
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-09-18 16:55:00 +0300

    Add files via upload

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  registerUser: any = {}; // You can remove this line
+
+  constructor(private formBuilder: FormBuilder,private authServ:AuthService) { }
+
+  ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      this.registerUser = Object.assign({}, this.registerForm.value);
+      this.authServ.register(this.registerUser);
+    }
+  }
+}
