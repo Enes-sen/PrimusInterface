dd1894cd44bc0a9fb989863637db6b13a1d59d66
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-09-18 14:30:20 +0300

    Create Alien.ts

---
+++ src/models/Alien.ts
@@ -0,0 +1,9 @@
+export class Alien{
+  id:Number;
+  homeWorld:string |null;
+  dnaSample:string |null;
+  givenName:string |null;
+  userId:number;
+  alienShadow:string|null;
+  formFile: File |null;
+}
